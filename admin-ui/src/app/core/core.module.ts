97ae662457c15f2623a0a764a32b957f5fed3125
manzede benard <manzede@gmail.com>
2022-12-05 12:48:56 +0300

    --amend

--- admin-ui/src/app/core/core.module.ts
+++ admin-ui/src/app/core/core.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
-import { HomeComponent } from './home/home.component';
-import { ParentComponent } from './parent/parent.component';
-import { RouterModule } from '@angular/router';
-import { MaterialModule } from '../shared/material.module';
-import { SideMenuService } from './services/side-menu.service';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { DataStorageService } from './services/data-storage.service';
-import { SharedModule } from '../shared/shared.module';
-import { AuthService } from './services/authservice.service';
-import { LoginRedirectService } from './services/loginredirect.service';
-import { AuthguardService } from './services/authguard.service';
-import { CanDeactivateGuardService } from './services/can-deactivate-guard.service';
-import { AuthInterceptor } from './services/httpinterceptor';
-import { CommonService } from './services/common.service';
-import { AuditService } from './services/audit.service';
-import { RolesService } from './services/roles.service';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { SideMenuComponent } from "./side-menu/side-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { ParentComponent } from "./parent/parent.component";
+import { RouterModule } from "@angular/router";
+import { MaterialModule } from "../shared/material.module";
+import { SideMenuService } from "./services/side-menu.service";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { DataStorageService } from "./services/data-storage.service";
+import { SharedModule } from "../shared/shared.module";
+import { AuthService } from "./services/authservice.service";
+import { LoginRedirectService } from "./services/loginredirect.service";
+import { AuthguardService } from "./services/authguard.service";
+import { CanDeactivateGuardService } from "./services/can-deactivate-guard.service";
+import { AuthInterceptor } from "./services/httpinterceptor";
+import { CommonService } from "./services/common.service";
+import { AuditService } from "./services/audit.service";
+import { RolesService } from "./services/roles.service";
+import { FooterComponent } from "./footer/footer.component";
+import { SpinnerComponent } from "./spinner/spinner.component";
 
 @NgModule({
   imports: [
@@ -27,15 +29,37 @@ import { RolesService } from './services/roles.service';
     HttpClientModule,
     SharedModule,
   ],
-  declarations: [HeaderComponent, SideMenuComponent, HomeComponent, ParentComponent],
-  exports: [HeaderComponent, SideMenuComponent, HomeComponent, ParentComponent, MaterialModule, RouterModule],
-  providers: [SideMenuService, DataStorageService, AuthService, LoginRedirectService, AuthguardService,
-    CanDeactivateGuardService, CommonService, AuditService, RolesService,
+  declarations: [
+    HeaderComponent,
+    SideMenuComponent,
+    HomeComponent,
+    ParentComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    SideMenuComponent,
+    HomeComponent,
+    ParentComponent,
+    MaterialModule,
+    RouterModule,
+    FooterComponent,
+    SpinnerComponent,
+  ],
+  providers: [
+    SideMenuService,
+    DataStorageService,
+    AuthService,
+    LoginRedirectService,
+    AuthguardService,
+    CanDeactivateGuardService,
+    CommonService,
+    AuditService,
+    RolesService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
